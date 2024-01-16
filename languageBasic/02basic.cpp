b54da30b325da0d793a57e599cb46e4e80a01985
Piyush289kumar <piyushraikwar289@gmail.com>
2024-01-16 21:04:12 +0530

    02Basic file

---
+++ languageBasic/02basic.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    cout << "Enter values for x and y : ";
+
+    double x;
+    double y;
+
+    cin >> x >> y;
+
+    cout << "Value of X : " << x << " Value of Y : " << y;
+
+    return 0;
+}
