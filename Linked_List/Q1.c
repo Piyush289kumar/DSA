5fe4dc7367018cf0c730ebb962a9d523ec46edbc
Piyush289kumar <piyushraikwar289@gmail.com>
2024-03-21 22:40:23 +0530

    Error on Runing C file

---
+++ Linked_List/Q1.c
@@ -0,0 +1,13 @@
+// Question 1: Find Length of a Linked List (Iterative and Recursive)
+
+#include<stdio.h> // header files  
+#include<conio.h>  
+void main()  
+{   // initialize the local variables.  
+    int l =5, b=10, ar, pr;  
+    printf("Length & Breadth of the rectangle is: %d & %d",l, b);  
+    ar = l * b; // calculate area of rectangle.  
+    pr = 2 * (l + b); // calculate perimeter of rectangle.  
+    printf("\n Area of Rectangle is: %d", ar);  
+    printf("\n Perimeter of Rectangle is: %d", pr);    
+}   
