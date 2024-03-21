4453f525c6be30e2840d4a01de5ed14e661c3c47
Piyush289kumar <piyushraikwar289@gmail.com>
2024-03-21 23:10:40 +0530

    DSA

--- Linked_List/Q1.c
+++ Linked_List/Q1.c
@@ -1,13 +1,55 @@
 // Question 1: Find Length of a Linked List (Iterative and Recursive)
 
-#include<stdio.h> // header files  
-#include<conio.h>  
-void main()  
-{   // initialize the local variables.  
-    int l =5, b=10, ar, pr;  
-    printf("Length & Breadth of the rectangle is: %d & %d",l, b);  
-    ar = l * b; // calculate area of rectangle.  
-    pr = 2 * (l + b); // calculate perimeter of rectangle.  
-    printf("\n Area of Rectangle is: %d", ar);  
-    printf("\n Perimeter of Rectangle is: %d", pr);    
-}   
+#include <stdio.h> // header files
+#include <stdlib.h>
+#include <conio.h>
+
+struct node
+{
+    int data;
+    struct node *prev, *next;
+};
+
+struct node *head = 0;
+struct node *tail = 0;
+
+void push(int elementAsParam)
+{
+    struct node *newnode = (struct node*)malloc(sizeof(struct node));
+    newnode->data = elementAsParam;
+
+    struct node *temp = head;
+
+    if (head == 0)
+    {
+        head = newnode;
+    }
+
+    while (temp->next != 0)
+    {
+        temp = temp->next;
+    }
+
+    temp->next = newnode;
+    newnode->prev = temp;
+}
+
+void display()
+{
+    struct node *tmp = head;
+    while (tmp == 0)
+    {
+        printf("\nElement is : %d", tmp->data);
+        tmp = tmp->next;
+    }
+}
+
+int main()
+{
+    push(3);
+    push(4);
+    push(6);
+    push(8);
+    display();
+    return 0;
+}
