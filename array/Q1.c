70f2a11ffd4e44f710ab823a969af5d7169634d1
Piyush289kumar <piyushraikwar289@gmail.com>
2024-04-10 20:56:46 +0530

    Question 1: Find The Lowest Value in an Array.

---
+++ array/Q1.c
@@ -0,0 +1,30 @@
+// Find The Lowest Value in an Array
+
+#include <stdio.h>
+
+int main()
+{
+
+    int inputArray[5] = {3, 43, 45, 2, 23};
+
+    int arrayLength = sizeof(inputArray) / sizeof(inputArray[0]);
+
+    int minVal = 0;
+
+    for (int idx = 0; idx < arrayLength; idx++)
+    {
+        if (minVal == 0)
+        {
+            minVal = inputArray[idx];
+        }
+
+        if (minVal > inputArray[idx])
+        {
+            minVal = inputArray[idx];
+        }
+    }
+
+    printf("MinVal : %d", minVal);
+
+    return 0;
+}
