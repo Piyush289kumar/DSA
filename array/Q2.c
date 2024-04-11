0e17cfd929eaa1e0359c338a3c57a9e56a1276b1
Piyush289kumar <piyushraikwar289@gmail.com>
2024-04-11 22:42:06 +0530

    Bubble Sort Algo

--- array/Q2.c
+++ array/Q2.c
@@ -1,27 +1,36 @@
 // Bubble Sort
 
 #include <stdio.h>
+#include <stdbool.h>
 
 int main()
 {
-    int inputArry[] = {7, 12, 9, 11, 3};
+    int inputArry[] = {3, 7, 12, 9, 11};
 
     int arrayLength = sizeof(inputArry) / sizeof(inputArry[0]);
 
-    for(int trm = 0; trm < arrayLength; trm++){
-        
-        for (int idx = 0; idx < arrayLength; idx++)
+    for (int trm = 0; trm < arrayLength; trm++)
     {
-        if(inputArry[idx] > inputArry[idx+1]){
-            int tempVal = inputArry[idx];
-            inputArry[idx] = inputArry[idx+1];
-            inputArry[idx+1] = tempVal;
+        bool isSwapAble = false;
+        for (int idx = arrayLength; idx >= 0; idx--)
+        {
+            if (inputArry[idx] > inputArry[idx + 1])
+            {
+                int tempVal = inputArry[idx];
+                inputArry[idx] = inputArry[idx + 1];
+                inputArry[idx + 1] = tempVal;
+                isSwapAble = true;
+            }
+        }
+        if (!isSwapAble)
+        {
+            break;
         }
     }
-    }
 
-    for(int idx = 0; idx<arrayLength; idx++){
-        printf("\n %d: " , inputArry[idx]);
+    for (int idx = 0; idx < arrayLength; idx++)
+    {
+        printf("\n %d: ", inputArry[idx]);
     }
     return 0;
 }
