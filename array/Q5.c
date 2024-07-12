aa4a3d4b9566c7b8ee857cd3d6ad65df88d60e1e
Piyush289kumar <piyushraikwar289@gmail.com>
2024-07-12 17:51:09 +0530

    Array reverse

---
+++ array/Q5.c
@@ -0,0 +1,16 @@
+// Q5: Write a program to reverse the array
+
+// Method 1: Create and Push element in new array.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int myArry[] = {2, 3, 4, 3, 2, 1};
+    int myArryLength = sizeof(myArry) / sizeof(myArry[0]);
+
+    int *newArry = (int *)malloc(myArryLength * sizeof(int));
+
+    
+}
