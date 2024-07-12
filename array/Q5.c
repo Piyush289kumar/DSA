fa06ab16802e2a49789048b40fcb9b3bdbd84483
Piyush289kumar <piyushraikwar289@gmail.com>
2024-07-12 18:10:24 +0530

    Reverse the array

--- array/Q5.c
+++ array/Q5.c
@@ -2,30 +2,57 @@
 // Method 1: Create and Push element in new array.
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+// int main()
+// {
+//     int myArry[] = {2, 3, 4, 3, 2, 1};
+//     int myArryLength = sizeof(myArry) / sizeof(myArry[0]);
+//     int *newArry = (int *)malloc(myArryLength * sizeof(int));
+//     if (newArry == NULL)
+//     {
+//         printf("Memory is not allocated");
+//         return 1;
+//     }
+//     for (int idx = 0; idx < myArryLength; idx++)
+//     {
+//         newArry[idx] = myArry[(myArryLength - 1) - idx];
+//     }
+//     printf("\n\nORG Array: ");
+//     for (int idx = 0; idx < myArryLength; idx++)
+//     {
+//         printf("[%d] ", myArry[idx]);
+//     }
+//     printf("\n\nREVERSE Array: ");
+//     for (int idx = 0; idx < myArryLength; idx++)
+//     {
+//         printf("[%d] ", newArry[idx]);
+//     }
+//     free(newArry);
+//     return 0;
+// }
+// Method 2: SWAP Elements
 int main()
 {
     int myArry[] = {2, 3, 4, 3, 2, 1};
     int myArryLength = sizeof(myArry) / sizeof(myArry[0]);
-    int *newArry = (int *)malloc(myArryLength * sizeof(int));
-    if (newArry == NULL)
-    {
-        printf("Memory is not allocated");
-        return 1;
-    }
-    for (int idx = 0; idx < myArryLength; idx++)
-    {
-        newArry[idx] = myArry[(myArryLength - 1) - idx];
-    }
-    printf("\n\nORG Array: ");
+    int midIdx = (int)ceil(myArryLength / 2);
+    int tempVal = 0;
+    printf("Mid Idx = %d", midIdx);
+    printf("\n\nORG ARRAY: ");
     for (int idx = 0; idx < myArryLength; idx++)
     {
         printf("[%d] ", myArry[idx]);
     }
-    printf("\n\nREVERSE Array: ");
+    for (int idx = 0; idx < midIdx; idx++)
+    {
+        tempVal = myArry[idx];
+        myArry[idx] = myArry[((myArryLength - 1) - idx)];
+        myArry[(myArryLength - 1) - idx] = tempVal;
+    }
+    printf("\n\nSWAP ARRAY: ");
     for (int idx = 0; idx < myArryLength; idx++)
     {
-        printf("[%d] ", newArry[idx]);
+        printf("[%d] ", myArry[idx]);
     }
-    free(newArry);
     return 0;
 }
