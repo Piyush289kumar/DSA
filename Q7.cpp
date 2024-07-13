0d2be52f7435bcf2ac2c3095ceb603d6ba360ec9
Piyush289kumar <piyushraikwar289@gmail.com>
2024-07-13 22:48:50 +0530

    Count number of occurrences (or frequency) in a sorted array.

--- Q7.cpp
+++ Q7.cpp
@@ -1,15 +1,10 @@
 // Q7: Count number of occurrences (or frequency) in a sorted array.
 #include <bits/stdc++.h>
-
 using namespace std;
-
 int countOccurences(int paramArr[], int sizeOfArray, int element)
 {
-
     sort(paramArr, paramArr + sizeOfArray);
-
     int count = 0;
-
     for (int idx = 0; idx < sizeOfArray; idx++)
     {
         if (element == paramArr[idx])
@@ -17,17 +12,14 @@ int countOccurences(int paramArr[], int sizeOfArray, int element)
             count++;
         }
     }
-
     return count;
 }
-
 int main()
 {
     int myArray[] = {1, 1, 2, 2, 2, 2, 3};
     int sizeOfArray = sizeof(myArray) / sizeof(myArray[0]);
     int element = 1;
     int occurencesCount = countOccurences(myArray, sizeOfArray, element);
-
     cout << "Element : " << element << " is occured in array " << occurencesCount << " times ";
     return 0;
 }
