66a3506e68a977f48c2bcd9c624e9fb7a454bee7
Piyush289kumar <piyushraikwar289@gmail.com>
2024-07-14 15:39:16 +0530

    Suck in Union & Intersection

---
+++ array/Q11.cpp
@@ -0,0 +1,30 @@
+// Q11: Find the Union and Intersection of the two sorted arrays.
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+#define LENTH_OF_ARR(arr) sizeof(arr) / sizeof(arr[0])
+
+int main()
+{
+
+    int arrA[] = {1, 3, 4, 5, 7};
+    int arrB[] = {2, 3, 5, 6};
+
+    int lenghtA = sizeof(arrA) / sizeof(arrA[0]);
+    int lenghtB = sizeof(arrB) / sizeof(arrB[0]);
+
+
+    for(int idx = 0; idx < lenghtA; idx++){
+
+        for (int i = 0; i < lenghtB; i++)
+        {
+            /* code */
+        }
+        
+
+    }
+}
