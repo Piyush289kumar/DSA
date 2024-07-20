905eef880c330c11092c3d699a4fef035997d4c8
Piyush289kumar <piyushraikwar289@gmail.com>
2024-07-20 11:41:13 +0530

    Rotate Matrix 90deg - Printing Done

---
+++ array/Q17.cpp
@@ -0,0 +1,43 @@
+/* Q17: 48. Rotate Matrix 90deg
+
+You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+Example 1:
+Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [[7,4,1],[8,5,2],[9,6,3]]
+Example 2:
+Input: matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+Output: [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
+Constraints:
+    n == matrix.length == matrix[i].length
+    1 <= n <= 20
+    -1000 <= matrix[i][j] <= 1000
+*/
+#include <bits/stdc++.h>
+using namespace std;
+void rotate(vector<vector<int>> &matrix)
+{
+    int n = matrix[0].size();
+    cout << "90 DEG MAXTRIX: " << endl;
+    cout << "[";
+    for (int colIdx = 0; colIdx < n; colIdx++)
+    {
+        cout << "[";
+        for (int rowIdx = n - 1; rowIdx >= 0; rowIdx--)
+        {
+            cout << matrix[rowIdx][colIdx];
+            if (rowIdx != 0)
+                cout << ", ";
+        }
+        cout << "]";
+        if (colIdx != n - 1)
+            cout << ", ";
+    }
+    cout << "]";
+}
+int main()
+{
+    vector<vector<int>> matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    rotate(matrix);
+    return 0;
+}
