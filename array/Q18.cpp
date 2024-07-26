84e79123210ae29374f526ef4780cfacd109d4ba
Piyush289kumar <piyushraikwar289@gmail.com>
2024-07-26 22:21:26 +0530

    Two Sum

--- array/Q18.cpp
+++ array/Q18.cpp
@@ -23,36 +23,31 @@ Constraints:
     -109 <= target <= 109
     Only one valid answer exists.
 */
-
 #include <bits/stdc++.h>
 using namespace std;
-
 vector<int> twoSum(vector<int> &nums, int target)
 {
     vector<int> ans;
     for (int i = 0; i < nums.size(); i++)
     {
-        for (int j = 0; j < nums.size(); j++)
+        for (int j = i + 1; j < nums.size(); j++)
         {
             if ((nums[i] + nums[j]) == target)
             {
-                ans.push_back(nums[i]);
-                ans.push_back(nums[j]);
+                ans.push_back(i);
+                ans.push_back(j);
+                return ans;
             }
         }
     }
-
     return ans;
 }
 int main()
 {
     vector<int> nums = {1, 2, 3, 4, 5};
     int target = 9;
-
     vector<int> ans = twoSum(nums, target);
-
     cout << "OUTPUT : " << endl;
-
     for (int i = 0; i < ans.size(); i++)
     {
         cout << ans[i] << " ";
