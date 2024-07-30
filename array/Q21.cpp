c1b472754841e63a3ab16f51e8558ff838a89093
Piyush289kumar <piyushraikwar289@gmail.com>
2024-07-30 23:34:59 +0530

    Search Insert Position

---
+++ array/Q21.cpp
@@ -0,0 +1,57 @@
+/*
+Q21: 35. Search Insert Position
+Easy
+Topics
+Companies
+Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+You must write an algorithm with O(log n) runtime complexity.
+Example 1:
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+Example 2:
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+Example 3:
+Input: nums = [1,3,5,6], target = 7
+Output: 4
+Constraints:
+    1 <= nums.length <= 104
+    -104 <= nums[i] <= 104
+    nums contains distinct values sorted in ascending order.
+    -104 <= target <= 104
+ */
+#include <bits/stdc++.h>
+using namespace std;
+int searchInsert(vector<int> &nums, int target)
+{
+    int firstIdx = 0;
+    int lastIdx = nums.size() - 1;
+    int middle = 0;
+    int idx = 0;
+    while (firstIdx <= lastIdx)
+    {
+        middle = lastIdx + (firstIdx - lastIdx) / 2;
+        if (nums[middle] == target)
+        {
+            return middle;
+        }
+        else if (nums[middle] < target)
+        {
+            idx = middle + 1;
+            firstIdx = middle + 1;
+        }
+        else if (nums[middle] > target)
+        {
+            idx = middle;
+            lastIdx = middle - 1;
+        }
+    }
+    return idx;
+}
+int main()
+{
+    vector<int> nums = {1, 3, 5, 6};
+    int target = 5;
+    int res = searchInsert(nums, target);
+    cout << "OUTPUT : " << res << endl;
+}
