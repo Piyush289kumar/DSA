1f3dc7d4b98fdedc80d469128e9993b5c6880d6d
Piyush289kumar <piyushraikwar289@gmail.com>
2024-08-03 21:46:42 +0530

    Problem init

---
+++ Recursion/Q6.cpp
@@ -0,0 +1,25 @@
+/* Q6: Reverse a string. */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+string reverseStr(string str, int start)
+{
+    int end = str.size()-1;
+
+    if (start == end)
+        return str;
+
+    swap(str[start], str[end]);
+
+    return reverseStr(str, start + 1);
+}
+
+int main()
+{
+    string s = "abc";
+    int first = 0;
+    string reversed = reverseStr(s, first);
+    cout << "OUTPUT : " << reversed;
+    return 0;
+}
