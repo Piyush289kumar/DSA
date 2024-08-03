5a4e7f8c1a5ae7f0db3ed12a6823c135c31400a4
Piyush289kumar <piyushraikwar289@gmail.com>
2024-08-03 22:15:13 +0530

    Not Solved

--- Recursion/Q7.cpp
+++ Recursion/Q7.cpp
@@ -1,35 +1,25 @@
 /* Q7: Bubble Sort Using Recursion. */
-
 #include <bits/stdc++.h>
 using namespace std;
-
 vector<int> bubbleSort(vector<int> &nums, int first, int last)
 {
-
     if (first == last)
         return nums;
-
-    int maxEl = nums[first];
-
+    int maxEl = nums[firist];
     while (first < last)
     {
         if (maxEl < nums[first])
             maxEl = nums[first];
-
         first++;
     }
     nums[last] = maxEl;
-
     return bubbleSort(nums, first, last - 1);
 }
-
 int main()
 {
-
     vector<int> nums = {4, 5, 6, 7, 8, 10, 2, 1};
     int first = 0;
     int last = nums.size() - 1;
-
     cout << "BEFORE ::::::: " << endl
          << endl;
     for (int idx = 0; idx < nums.size() - 1; idx)
@@ -38,15 +28,12 @@ int main()
     }
     cout << endl
          << endl;
-
     bubbleSort(nums, first, last);
-
     cout << "AFTER ::::::: " << endl
          << endl;
     for (int idx = 0; idx < nums.size() - 1; idx)
     {
         cout << idx << " - " << nums[idx] << endl;
     }
-
     return 0;
 }
