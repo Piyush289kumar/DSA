4e15162be04809525dbc872908aaedc7f0943cdd
Piyush289kumar <piyushraikwar289@gmail.com>
2024-08-03 22:14:20 +0530

    Stuck

--- Recursion/Q7.cpp
+++ Recursion/Q7.cpp
@@ -3,12 +3,50 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+vector<int> bubbleSort(vector<int> &nums, int first, int last)
+{
 
-vector<int> bubbleSort(vector<int>)
+    if (first == last)
+        return nums;
+
+    int maxEl = nums[first];
+
+    while (first < last)
+    {
+        if (maxEl > nums[first])
+            maxEl = nums[first];
+
+        first++;
+    }
+    nums[last] = maxEl;
+
+    return bubbleSort(nums, first, last - 1);
+}
 
 int main()
 {
 
     vector<int> nums = {4, 5, 6, 7, 8, 10, 2, 1};
+    int first = 0;
+    int last = nums.size() - 1;
+
+    cout << "BEFORE ::::::: " << endl
+         << endl;
+    for (int idx = 0; idx < nums.size() - 1; idx)
+    {
+        cout << idx << " - " << nums[idx] << endl;
+    }
+    cout << endl
+         << endl;
+
+    bubbleSort(nums, first, last);
+
+    cout << "AFTER ::::::: " << endl
+         << endl;
+    for (int idx = 0; idx < nums.size() - 1; idx)
+    {
+        cout << idx << " - " << nums[idx] << endl;
+    }
+
     return 0;
 }
