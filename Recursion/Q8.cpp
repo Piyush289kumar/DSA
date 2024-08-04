5a4be5d46f83b43edc13fb79d559800aafb206a6
Piyush289kumar <piyushraikwar289@gmail.com>
2024-08-04 22:39:39 +0530

    Calculate n^m.

--- Recursion/Q8.cpp
+++ Recursion/Q8.cpp
@@ -3,16 +3,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int calculatePower(int n, int m)
+{
 
-int calculatePower(int n, int m){
+    if (m == 0)
+        return 1;
 
+    if (m == 1)
+        return n;
+
+    int ans = pow(n, m / 2);
+
+    if (m % 2 == 0)
+        return ans * ans;
+    else
+        return n * ans * ans;
 }
-
 
 int main()
 {
-    int n = 2;
-    int m = 2;
+    int n = 3;
+    int m = 10;
     int result = calculatePower(n, m);
 
     cout << "OUTPUT : " << result;
