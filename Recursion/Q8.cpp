217eb9c5285098d50a938b3248f6535b81f9c278
Piyush289kumar <piyushraikwar289@gmail.com>
2024-08-04 22:31:35 +0530

    "Added function signature and main function for calculating n^m, but implementation of calculatePower function is missing."

--- Recursion/Q8.cpp
+++ Recursion/Q8.cpp
@@ -1,1 +1,19 @@
 /* Q8: Calculate n^m. */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int calculatePower(int n, int m){
+
+}
+
+
+int main()
+{
+    int n = 2;
+    int m = 2;
+    int result = calculatePower(n, m);
+
+    cout << "OUTPUT : " << result;
+}
