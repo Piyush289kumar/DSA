91ccdd862c0c046456512c6635457b0ae1412d4d
Piyush@289 <piyushraikwar289@gmail.com>
2024-08-05 11:32:00 +0530

    Simple Bubble Sort Done

--- Recursion/Q7.cpp
+++ Recursion/Q7.cpp
@@ -2,3 +2,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+vector<int> bubbleSort(vector<int> &nums)
+{
+    int n = nums.size();
+
+    for (int idx = n - 1; idx > 0; idx--)
+    {
+        for (int j = 0; j < idx; j++)
+        {
+            if (nums[j] > nums[j + 1])
+                swap(nums[j], nums[j + 1]);
+        }
+    }
+
+    return nums;
+}
+
+int main()
+{
+
+    vector<int> nums = {1, 2, 4, 8, 6, 3, 1};
+
+    vector<int> ans = bubbleSort(nums);
+
+    for (int idx = 0; idx < ans.size(); idx++)
+    {
+        cout << " - " << ans[idx];
+    }
+    return 0;
+}
