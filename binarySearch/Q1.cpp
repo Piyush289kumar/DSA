969a04262aa36ebd0d06fd4995ac566f31906a32
Piyush289kumar <piyushraikwar289@gmail.com>
2024-08-07 15:44:47 +0530

    Problem init

--- binarySearch/Q1.cpp
+++ binarySearch/Q1.cpp
@@ -21,3 +21,42 @@ Constraints:
     nums is a non-decreasing array.
     -109 <= target <= 109
 */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int findFirstIdx(vector<int> &nums, int target, int first, int last)
+{
+    return -1;
+}
+int findLastIdx(vector<int> &nums, int target, int first, int last)
+{
+    return -1;
+}
+
+vector<int> findFirstAndLastIndex(vector<int> &nums, target)
+{
+    vector<int> response = {0, 0};
+    int first = 0;
+    int last = nums.size() - 1;
+
+    response[0] = findFirstIdx(nums, target, first, last);
+    response[0] = findLastIdx(nums, target, first, last);
+
+    return response;
+}
+
+int main()
+{
+    vector<int> nums = {0, 5, 5, 6, 6, 6};
+    int target = 5;
+    vector<int> ans = findFirstAndLastIndex(nums, target);
+
+    cout << "OUTPUT :" << endl;
+
+    cout << "FIRST IDX : " << ans[0] << " | LAST IDX : " << ans[1];
+
+    return 0;
+}
