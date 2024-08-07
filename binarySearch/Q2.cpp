ffe4b2275505f8815ba6384cc0287ff1f5ac5b8d
Piyush289kumar <piyushraikwar289@gmail.com>
2024-08-07 17:25:00 +0530

    Test Case Failed

--- binarySearch/Q2.cpp
+++ binarySearch/Q2.cpp
@@ -22,6 +22,47 @@ Constraints:
 
 #include <iostream>
 #include <vector>
+using namespace std;
+
+int findPeakEle(vector<int> arr, int peakEle, int first, int end)
+{
+
+    if (first > end)
+    {
+        return peakEle;
+    }
+
+    int mid = end + (first - end) / 2;
+
+    if (arr[mid] > arr[mid - 1])
+    {
+        return findPeakEle(arr, mid, mid + 1, end);
+    }
+    if (arr[mid] > arr[mid + 1])
+    {
+        return findPeakEle(arr, mid, first, mid - 1);
+    }
+}
 int peakIndexInMountainArray(vector<int> &arr)
 {
+    if (arr.size() < 3 || arr.size() > 105)
+        return -1;
+
+    int first = 0;
+    int end = arr.size() - 1;
+    int peakEle = -1;
+
+    int res = findPeakEle(arr, peakEle, first, end);
+
+    return res;
+}
+
+int main()
+{
+    vector<int> arr = {0, 1, 0};
+
+    int ans = peakIndexInMountainArray(arr);
+
+    cout << "OUTPUT : " << ans;
+    return 0;
 }
