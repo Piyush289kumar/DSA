97686d08ab9b9f4d7c3876f7da76873aaedd4fb5
Piyush289kumar <piyushraikwar289@gmail.com>
2024-08-08 22:49:38 +0530

    Find the Pivot Element. Time Complexity O(log n)

--- binarySearch/Q3.cpp
+++ binarySearch/Q3.cpp
@@ -1,15 +1,20 @@
 /* Q3: Find the Pivot Element. Time Complexity O(log n)*/
-
 #include <iostream>
 #include <vector>
-
 using namespace std;
-
-int findPivotElement(vector<int> &nums, int start, int end) {}
-
+int findPivotElement(vector<int> &nums, int start, int end)
+{
+    if (start == end)
+        return end;
+    int mid = (start + end) / 2;
+    if (nums[0] < nums[mid])
+        return findPivotElement(nums, mid + 1, end);
+    else
+        return findPivotElement(nums, start, mid);
+}
 int main()
 {
-    vector<int> nums = {4, 5, 6, 7, 0, 1, 2};
+    vector<int> nums = {4, 5, 6, 0, 1, 2};
     int start = 0;
     int end = nums.size() - 1;
     cout << "OUTPUT : " << findPivotElement(nums, start, end);
