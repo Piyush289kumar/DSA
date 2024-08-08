3cd838faf5f94e05744bfecbf6d75ff99ac13ea5
Piyush289kumar <piyushraikwar289@gmail.com>
2024-08-08 22:44:27 +0530

    Problem init

---
+++ binarySearch/Q3.cpp
@@ -0,0 +1,17 @@
+/* Q3: Find the Pivot Element. Time Complexity O(log n)*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int findPivotElement(vector<int> &nums, int start, int end) {}
+
+int main()
+{
+    vector<int> nums = {4, 5, 6, 7, 0, 1, 2};
+    int start = 0;
+    int end = nums.size() - 1;
+    cout << "OUTPUT : " << findPivotElement(nums, start, end);
+    return 0;
+}
