18966ead48c354630dc79a3e4700d5f376ea998f
Piyush289kumar <piyushraikwar289@gmail.com>
2024-08-10 23:09:37 +0530

    "Added functions for finding pivot index and binary search in a rotated array, and modified main function to include a target value."

--- binarySearch/Q4.cpp
+++ binarySearch/Q4.cpp
@@ -5,9 +5,46 @@
 
 using namespace std;
 
+int findPivotIdx(vector<int> &nums, int start, int end)
+{
+    if (start == end)
+        return start;
+
+    int mid = (start + end) / 2;
+
+    if (nums[mid] < nums[mid + 1])
+        return findPivotIdx(nums, mid + 1, end);
+    else
+        return findPivotIdx(nums, start, mid);
+}
+
+int binarySearch(vector<int> &nums, int target, int start, int end)
+{
+    if (start > end)
+        return -1;
+
+    int mid = (start + end) / 2;
+
+    if (nums[mid] == target)
+}
+
+int searchInRotatedArr(vector<int> &nums, int target)
+{
+
+    int start = 0;
+    int end = nums.size() - 1;
+    int pivotIdx = findPivotIdx(nums, start, end);
+
+    if (nums[start] <= target && target <= nums[pivotIdx])
+        return binarySearch(nums, target, start, pivotIdx - 1);
+    else
+        return binarySearch(nums, target, pivotIdx, end);
+}
+
 int main()
 {
     vector<int> nums = {7, 9, 0, 1, 2, 3};
+    int target = 9;
 
     return 0;
 }
