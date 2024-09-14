59719927e9581673d51948559f655aca27728f7c
Piyush289kumar <piyushraikwar289@gmail.com>
2024-09-14 21:51:58 +0530

    Boiler Code

---
+++ math/Q3.cpp
@@ -0,0 +1,22 @@
+/* Q3:  Modular Exponentiation - Coding Ninjas
+Easy
+
+Problem statement
+
+You are given a three integers 'X', 'N', and 'M'. Your task is to find ('X' ^ 'N') % 'M'. A ^ B is defined as A raised to power B and A % C is the remainder when A is divided by C.
+Detailed explanation ( Input/output format, Notes, Images )
+Constraints :
+
+1 <= T <= 100   
+
+1 <= X, N, M <= 10^9
+Time limit: 1 sec
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    return 0;
+}
