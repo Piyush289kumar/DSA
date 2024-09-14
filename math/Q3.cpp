45be522418ca0e49046775bc2d84bd8794446cd5
Piyush289kumar <piyushraikwar289@gmail.com>
2024-09-14 22:01:14 +0530

    Local Test passs

--- math/Q3.cpp
+++ math/Q3.cpp
@@ -7,16 +7,42 @@ You are given a three integers 'X', 'N', and 'M'. Your task is to find ('X' ^ 'N
 Detailed explanation ( Input/output format, Notes, Images )
 Constraints :
 
-1 <= T <= 100   
+1 <= T <= 100
 
 1 <= X, N, M <= 10^9
 Time limit: 1 sec
 */
 
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-int main(){
+int modularExponentiation(int x, int n, int m)
+{
+
+    int response = 1;
+
+    x = x % m;
+
+    while (n > 0)
+    {
+        if (n % 2 != 0)
+        {
+            response = (1LL * response * x) % m;
+        }
+        x = (1LL * x * x) % m;
+        n = n / 2;
+    }
+    return response;
+}
+
+int main()
+{
+
+    int x = 4;
+    int n = 3;
+    int m = 10;
+
+    cout << "RESPONSE ==> " << modularExponentiation(x, n, m);
     return 0;
 }
