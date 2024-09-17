a4ef67be3e848c649d255bcb4c0e49014c7574c1
Piyush289kumar <piyushraikwar289@gmail.com>
2024-09-17 23:05:53 +0530

    "Commented out variable declarations and print statements in main function."

--- extra/ReferenceVariable.cpp
+++ extra/ReferenceVariable.cpp
@@ -5,11 +5,11 @@ using namespace std;
 int main()
 {
 
-    int i = 10;
-    int &j = i;
+    // int i = 10;
+    // int &j = i;
 
-    cout << "I ==> " << i << endl;
-    cout << "J ==> " << j << endl;
+    // cout << "I ==> " << i << endl;
+    // cout << "J ==> " << j << endl;
 
     return 0;
 
