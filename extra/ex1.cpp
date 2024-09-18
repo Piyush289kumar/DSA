aacec47e25a8f6ca736790ae809db4ec199398a5
Piyush289kumar <piyushraikwar289@gmail.com>
2024-09-18 22:01:07 +0530

    Macro

--- extra/ex1.cpp
+++ extra/ex1.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 
-#define a 1;
+#define A 2
+
 using namespace std;
 
-inline int getMax(int &a, int &b)
+inline int getMax(int a, int& b)
 {
     return (a > b) ? a : b;
 }
 int main()
 {
 
-    // int a = 1;
     int b = 2;
 
-    cout << "RESPONSE ==> " << getMax(a,b);
+    cout << "RESPONSE ==> " << getMax(A, b);
 
     return 0;
 }
