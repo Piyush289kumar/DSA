fb323230d9b1e5f7e4385284527fb1319e1999df
Piyush289kumar <piyushraikwar289@gmail.com>
2024-09-18 22:01:02 +0530

    Define

--- extra/ex1.cpp
+++ extra/ex1.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
 
+#define a 1;
 using namespace std;
 
+inline int getMax(int &a, int &b)
+{
+    return (a > b) ? a : b;
+}
 int main()
 {
 
-    // int i = 10;
-    // int &j = i;
+    // int a = 1;
+    int b = 2;
 
-    // cout << "I ==> " << i << endl;
-    // cout << "J ==> " << j << endl;
+    cout << "RESPONSE ==> " << getMax(a,b);
 
     return 0;
-
 }
