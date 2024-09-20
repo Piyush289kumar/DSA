0fc38a1ac478ffc9dadbea77c116da12222842d6
Piyush289kumar <piyushraikwar289@gmail.com>
2024-09-20 15:51:37 +0530

    Reverse a String.

--- Recursion/Q15.cpp
+++ Recursion/Q15.cpp
@@ -8,19 +8,35 @@ Example : 1. "abcde" => "edcba".
 
 using namespace std;
 
-string reverseString(string str)
+// Solution 1
+// string reverseString(string str)
+// {
+//     if (str.length() == 0)
+//         return str;
+
+//     char lastChar = str.back();
+//     str.pop_back();
+//     return lastChar + reverseString(str);
+// }
+
+// Solution 2
+
+string reverseString(string &str, int start, int end)
 {
-    if (str.length() == 0)
+    if (start >= end)
         return str;
 
-    char lastChar = str.back();
-    str.pop_back();
-    return lastChar + reverseString(str);
+    swap(str[start], str[end]);
+    start++;
+    end--;
+    return reverseString(str, start, end);
 }
 
 int main()
 {
     string str = "abcde";
-    cout << "RESPONSE ==> " << reverseString(str);
+    int start = 0;
+    int end = str.length() - 1;
+    cout << "RESPONSE ==> " << reverseString(str, start, end);
     return 0;
 }
