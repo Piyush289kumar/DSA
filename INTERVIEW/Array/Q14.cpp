cc2a251af454f6c8476b8c04b93a92f35abccf07
Piyush289kumar <piyushraikwar289@gmail.com>
2024-10-25 12:54:06 +0530

    Q14: Kth Smallest and Largest Element of Array

--- INTERVIEW/Array/Q14.cpp
+++ INTERVIEW/Array/Q14.cpp
@@ -1,22 +1,68 @@
-/* Q14.1: Kth Smallest
-Difficulty: MediumAccuracy: 35.17%Submissions: 650K+Points: 4
+/* Q14: Kth Smallest and Largest Element of Array
+Easy
+0/40
+Average time to solve is 15m
+Contributed by
+266 upvotes
+Asked in companies
+Problem statement
 
-Given an array arr[] and an integer k where k is smaller than the size of the array, the task is to find the kth smallest element in the given array.
+You are given an array ‘Arr’ consisting of ‘N’ distinct integers and a positive integer ‘K’. Find out Kth smallest and Kth largest element of the array. It is guaranteed that K is not greater than the size of the array.
 
-Follow up: Don't solve it using the inbuilt sort function.
+Example:
 
-Examples :
+Let ‘N’ = 4,  ‘Arr’ be [1, 2, 5, 4] and ‘K’ = 3.
+then the elements of this array in ascending order is [1, 2, 4, 5].  Clearly, the 3rd smallest and largest element of this array is 4 and 2 respectively.
 
-Input: arr[] = [7, 10, 4, 3, 20, 15], k = 3
-Output:  7
-Explanation: 3rd smallest element in the given array is 7.
+Detailed explanation ( Input/output format, Notes, Images )
+Constraints:
 
-Input: arr[] = [2, 3, 1, 20, 15], k = 4
-Output: 15
-Explanation: 4th smallest element in the given array is 15.
+1 <= T <= 50
+1 <= N <= 10^4
+1 <= K <= N
+-10^9 <= Arr[i] <= 10^9
 
-Expected Time Complexity: O(n+(max_element) )
-Expected Auxiliary Space: O(max_element)
+Where ‘T’ is the total number of test cases, ‘N’ is the size of array ‘Arr’ and Arr[i] is the element of the given array.
+
+Time limit: 1 sec
+
+Sample Input 1:
+
+2
+4 4
+5 6 7 2
+4 3
+1 2 5 4
+
+Sample Output 1:
+
+7 2
+4 2
+
+Explanation of Sample Input 1:
+
+Test case 1:
+Here, ‘N’ = 4, ‘Arr’ = [5, 6, 7, 2] and ‘K’ = 3.
+Elements of the array in ascending order are [2, 5, 6, 7]
+Thus the 4rd smallest and 4rd largest elements of this array are 7 and 2 respectively.
+
+Test case 2:
+See problem statement for an explanation.
+
+Sample Input 2:
+
+ 2
+ 1 1
+ 2
+ 5 1
+ 5 4 3 2 1
+
+Sample Output 2:
+
+ 2 2
+ 1 5
+
+
 */
 
 #include <iostream>
@@ -25,36 +71,59 @@ Expected Auxiliary Space: O(max_element)
 
 using namespace std;
 
-int kthSmallest(vector<int> &arr, int k)
+int findKthSmallestElement(vector<int> arr, int n, int k)
 {
-    int n = arr.size();
-
     priority_queue<int> maxHeap;
-
-    for (int i = 0; i < k; i++)
+    for (int idx = 0; idx < k; idx++)
     {
-        maxHeap.push(arr[i]);
+        maxHeap.push(arr[idx]);
     }
-
-    for (int i = k; i < n; i++)
+    for (int idx = k; idx < n; idx++)
     {
-        if (arr[i] < maxHeap.top())
+        if (arr[idx] < maxHeap.top())
         {
             maxHeap.pop();
-            maxHeap.push(arr[i]);
+            maxHeap.push(arr[idx]);
         }
     }
-
     return maxHeap.top();
 }
+int findKthLargestElement(vector<int> arr, int n, int k)
+{
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+    for (int idx = 0; idx < k; idx++)
+    {
+        minHeap.push(arr[idx]);
+    }
+    for (int idx = k; idx < n; idx++)
+    {
+        if (arr[idx] > minHeap.top())
+        {
+            minHeap.pop();
+            minHeap.push(arr[idx]);
+        }
+    }
+    return minHeap.top();
+}
+vector<int> kthSmallLarge(vector<int> &arr, int n, int k)
+{
+    int kthSmallElement = findKthSmallestElement(arr, n, k);
+    int kthLargestElement = findKthLargestElement(arr, n, k);
 
+    vector<int> res(2);
+    res[0] = kthSmallElement;
+    res[1] = kthLargestElement;
+
+    return res;
+}
 int main()
 {
-
-    vector<int> arr = {7, 10, 4, 3, 20, 15};
+    vector<int> arr = {1, 2, 4, 5};
+    int n = arr.size();
     int k = 3;
-
-    cout << "RESONSE ==> " << kthSmallest(arr, k);
-
+    vector<int> res = kthSmallLarge(arr, n, k);
+    cout << "== RSEPONSE == " << endl
+         << "Smallest : " << res[0] << endl
+         << "Largets : " << res[1];
     return 0;
 }
