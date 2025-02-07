2620086dd1ab51123c1d6f17dec3de7853b0f878
Piyush289kumar <piyushraikwar289@gmail.com>
2025-02-07 17:12:11 +0530

    Q7.

---
+++ TCS/PYQ/OPTUS SHEET/2022/Q6.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void getFare(string Source, string Destination)
+{
+    unordered_map<string, string> busStops = {
+        {"TH", "THANERAILWAYSTN"},
+        {"GA", "GAONDEVI"},
+        {"IC", "ICEFACTROY"},
+        {"HA", "HARINIWASCIRCLE"},
+        {"TE", "TEENHATHNAKA"},
+        {"LU", "LUISWADI"},
+        {"NI", "NITINCOMPANYJUNCTION"},
+        {"CA", "CADBURRYJUNCTION"}
+    };
+
+    
+}
+
+int main()
+{
+    getFare("s", "d");
+    return 0;
+}
