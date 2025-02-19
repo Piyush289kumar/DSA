1f11d0b08952740dde6022f3bc2268e0f86a49dd
Piyush289kumar <piyushraikwar289@gmail.com>
2025-02-19 14:36:39 +0530

    Q8: TCS NQT Coding Question – 8 Problem Statement – An international round table conference will be held in india. Presidents from all over the world representing their respective countries will be attending the conference. The task is to find the possible number of ways(P) to make the N members sit around the circular table such that. The president and prime minister of India will always sit next to each other.

---
+++ TCS/PYQ/SRIT SHEET/Q8.cpp
@@ -0,0 +1,52 @@
+/* Q8: TCS NQT Coding Question – 8
+Problem Statement – An international round table conference will be held in india. Presidents from all over
+the world representing their respective countries will be attending the conference. The task is to find the
+possible number of ways(P) to make the N members sit around the circular table such that. The president
+and prime minister of India will always sit next to each other.
+Example 1: Input : 4 -> Value of N(No. of members)
+Output : 12 -> Possible ways of seating the members
+Explanation: 2 members should always be next to each other. So, 2 members can be in 2! ways
+Rest of the members can be arranged in (4-1)! ways.(1 is subtracted because the previously selected two
+members will be considered as single members now). So total possible ways 4 members can be seated
+around the circular table 2*6= 12. Hence, output is 12.
+Example 2: Input: 10 -> Value of N(No. of members)
+Output : 725760 -> Possible ways of seating the members
+Explanation: 2 members should always be next to each other. So, 2 members can be in 2! ways
+Rest of the members can be arranged in (10-1)! Ways. (1 is subtracted because the previously selected two
+members will be considered as a single member now). So, total possible ways 10 members can be seated
+around a round table is 2*362880 = 725760 ways. Hence, output is 725760.
+Constraints : 2<=N<=50
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+long long int fact(int N)
+{
+    if (N == 1)
+    {
+        return 1;
+    }
+    return N * fact(N - 1);
+}
+
+long long int waysOfSitingArg(int N)
+{
+    return (fact(N - 1) * 2);
+}
+
+int main()
+{
+    int N;
+    cin >> N;
+    if (N < 2 || N > 50)
+    {
+        cout << "INVALID INPUT" << endl;
+    }
+    else
+    {
+        cout << waysOfSitingArg(N) << endl;
+    }
+
+    return 0;
+}
