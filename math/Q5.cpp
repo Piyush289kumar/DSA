28421909833b0ce873a166a99dde75ad9208a3ff
Piyush289kumar <piyushraikwar289@gmail.com>
2025-03-10 22:06:52 +0530

    Q5: Decimal to Binary. s

---
+++ math/Q5.cpp
@@ -0,0 +1,27 @@
+/* Q5: Decimal to Binary. */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int binaryToDecimal(string N)
+{
+    int decimal = 0, len = N.size();
+
+    for (int i = 0; i < len; i++)
+    {
+        int n = N[len - 1 - i] - '0';
+        decimal += n * pow(2, i);
+    }
+
+    return decimal;
+}
+
+int main()
+{
+    string N;
+    cin >> N;
+
+    cout << binaryToDecimal(N) << endl;
+
+    return 0;
+}
