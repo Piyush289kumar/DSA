3311e9039ef8c7361e2adc5c7ac1cd836aa4de7a
Piyush289kumar <piyushraikwar289@gmail.com>
2025-04-05 20:45:40 +0530

    In,Pre,Post-Order

--- tree/basic/Q1.cpp
+++ tree/basic/Q1.cpp
@@ -77,26 +77,83 @@ void levelOrderTraversal(treeNode *root)
 
     // Print Reverse Tree;
 
-    cout << endl << "REV" << endl;
+    cout << endl
+         << "REV" << endl;
 
     for (int i = levels.size() - 1; i >= 0; i--)
     {
-        for(int val : levels[i]){
+        for (int val : levels[i])
+        {
             cout << val << " ";
         }
         cout << endl;
     }
 }
 
+void inorder(treeNode *root)
+{
+    if (!root)
+        return;
+
+    // inorder - LNR
+
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+
+
+void preorder(treeNode *root)
+{
+    if (!root)
+        return;
+
+    // preorder - NLR
+
+    cout << root->data << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+
+void postorder(treeNode *root)
+{
+    if (!root)
+        return;
+
+    // postorder - NLR
+
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->data << " ";
+}
+
+
 int main()
 {
     // data : 1 4 2 10 -1 -1 -1 8 -1 -1 3 6 -1 -1 7 -1 -1
+    // data2 : 1 3  7 -1 -1 11 -1 -1 5 17 -1 -1 -1
     treeNode *root = NULL;
 
     root = buildTree(root);
 
     cout << "BFS : " << endl;
     levelOrderTraversal(root);
+
+    cout << endl
+         << "In-Order" << endl;
+    inorder(root);
+
+
+    cout << endl
+         << "Pre-Order" << endl;
+    preorder(root);
+
+
+    cout << endl
+         << "Post-Order" << endl;
+    postorder(root);
 
     return 0;
 }
