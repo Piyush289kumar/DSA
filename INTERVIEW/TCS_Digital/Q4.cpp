f33fda9e4feaebf332df2a1c6a6b0eb766bf798e
Piyush289kumar <piyushraikwar@gmail.com>
2025-06-10 13:34:13 +0530

    Q4.Triangle Pattern.

--- INTERVIEW/TCS_Digital/Q4.cpp
+++ INTERVIEW/TCS_Digital/Q4.cpp
@@ -5,20 +5,32 @@ using namespace std;
 
 void triangle(int N)
 {
-
     for (int i = 1; i <= N; i++)
     {
-
         for (int j = 1; j <= N - i; j++)
         {
             cout << " ";
         }
-
         for (int s = 1; s <= ((2 * i) - 1); s++)
         {
             cout << "*";
         }
+        cout << endl;
+    }
+}
 
+void revTriangle(int N)
+{
+    for (int i = N; i >= 1; i--)
+    {
+        for (int j = (N - i); j >= 1; j--)
+        {
+            cout << " ";
+        }
+        for (int s = ((2 * i) - 1); s >= 1; s--)
+        {
+            cout << "*";
+        }
         cout << endl;
     }
 }
@@ -29,6 +41,8 @@ int main()
     cin >> N;
 
     triangle(N);
+    cout << endl;
+    revTriangle(N);
 
     return 0;
 }
