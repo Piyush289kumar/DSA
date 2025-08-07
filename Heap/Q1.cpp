e2bf487dd1e47be9c4f0b2b0f848314933b38dbc
Piyush289kumar <piyushraikwar289@gmail.com>
2025-08-07 09:20:59 +0530

    Delete;

--- Heap/Q1.cpp
+++ Heap/Q1.cpp
@@ -7,6 +7,28 @@ private:
     vector<int> nums;
     int size;
 
+    void heapify(int i)
+    {
+        int largest = i;
+        int left = 2 * i;
+        int right = 2 * i + 1;
+
+        if (left <= size && nums[largest] < nums[left])
+        {
+            largest = left;
+        }
+        if (right <= size && nums[largest] < nums[right])
+        {
+            largest = right;
+        }
+
+        if (largest != i)
+        {
+            swap(nums[i], nums[largest]);
+            heapify(largest);
+        }
+    }
+
 public:
     Heap()
     {
@@ -35,6 +57,19 @@ public:
         }
     }
 
+    void deleteNode()
+    {
+        if (size == 0)
+            return;
+        // 1. swap last left & root node
+        nums[1] = nums[size];
+        nums.pop_back();
+        size--;
+
+        // 2. Verify Heap
+        heapify(1);
+    }
+
     void print()
     {
         cout << "HEAP : " << endl;
@@ -57,5 +92,9 @@ int main()
     h.insert(10);
 
     h.print();
+
+    h.deleteNode();
+    h.print();
+
     return 0;
 }
